4204788cad7f68547bd3825b14c02def35f8f507
KhomichNastya <nastya0811h@mail.ru>
2023-01-11 01:48:10 +0300

    read_input_functions

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 #include <vector>
-#include <set>
 
-std::string ReadLine();
-int ReadLineWithNumber();
 std::vector<std::string> SplitIntoWords(const std::string& text);
 
